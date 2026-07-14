cb7f6ff05c6df37e9fc41a75d451fc33fae6283b
Tymofii Arsonov <tarsonov@edu.cdv.pl>
2026-07-14 14:07:43 +0200

    Initial Commit

---
+++ LocAI/LocAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocAIApp.swift
+//  LocAI
+//
+//  Created by Tymofii Arsonov on 14.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct LocAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
